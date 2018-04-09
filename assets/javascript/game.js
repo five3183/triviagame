666a7ccc8deae6f3c29c7cf954961f2f9a3cc4e0
five3183@gmail.com <five3183@gmail.com>
2018-04-09 19:33:14 -0400

    skeleton

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+questionArray =  ["test"];
+answerArray =  ["test"];
+var timeRemaining;
+var answersCorrect;
+var answersIncorrect;
+
+
+
+
+
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#QUESTION").text(questionArray);
+
+        alert("test")
+    }, 1000 * 30)
+
+});
